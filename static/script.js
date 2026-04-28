a7eef0e8cb84617e506971a7fad8d197a4e2650d
DavidStAF <davidstfarmer@gmail.com>
2026-04-28 16:56:50 -0400

    Added HTML and JS files

---
+++ static/script.js
@@ -0,0 +1,32 @@
+function sendData() {
+
+    let x = document.getElementById("xdata").value.split(",").map(Number);
+    let y = document.getElementById("ydata").value.split(",").map(Number);
+
+    fetch("/fit", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            x: x,
+            y: y,
+            model: document.getElementById("model").value,
+            title: document.getElementById("title").value,
+            xlabel: document.getElementById("xlabel").value,
+            ylabel: document.getElementById("ylabel").value
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+
+        let resultsDiv = document.getElementById("results");
+        resultsDiv.innerHTML = "";
+
+        data.parameters.forEach(p => {
+            resultsDiv.innerHTML += 
+                `${p.name} = ${p.value.toFixed(5)} ± ${p.uncertainty.toFixed(5)}<br>`;
+        });
+
+        let graph = JSON.parse(data.graph);
+        Plotly.newPlot("plot", graph.data, graph.layout);
+    });
+}
